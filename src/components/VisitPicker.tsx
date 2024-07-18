86c8a38155641368b5027c8587b2a35ef9b716db
williamwoldum <wwoldu21@student.aau.dk>
2024-07-19 00:00:41 +0200

    Small scroll sync fix

--- src/components/VisitPicker.tsx
+++ src/components/VisitPicker.tsx
@@ -144,7 +144,7 @@ export default function VisitPicker() {
         scrollRightDiv.removeEventListener('scroll', syncScroll as EventListener)
       }
     }
-  }, [])
+  }, [hidden])
 
   return (
     <>
